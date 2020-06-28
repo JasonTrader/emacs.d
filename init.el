e85731e0f03a15fb52a96cef5fbe684566078a8d
Jason Trader <jasonmtrader@gmail.com>
2020-06-28 02:03:23 -0400

    Update emacs init file.

--- init.el
+++ init.el
@@ -7,5 +7,17 @@
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
-(load-theme 'spacemacs-dark t)
+(load-theme 'dracula t)
 (setq inhibit-startup-screen t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (dracula-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
